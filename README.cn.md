a15ca294d7af80d815c70657e30137fac885383c
ophub <chinaue@gmail.com>
2021-04-21 13:49:22 +0800

    update

---
+++ README.cn.md
@@ -0,0 +1,74 @@
+# Flippy 的 OpenWrt 打包脚本 Actions
+
+## 功能说明
+
+完全使用 [Flippy 原站](https://github.com/unifreq/openwrt_packit) 的打包脚本，不做任何脚本修改，仅进行了智能化 Action 应用开发，让打包操作变得更加简单化和个性化。
+
+## 使用方法
+
+在 `.github/workflows/*.yml` 云编译脚本中引入此 Actions 即可使用，例如 [build-openwrt-armvirt.yml](https://github.com/ophub/op/blob/main/.github/workflows/build-openwrt-armvirt.yml) ，代码如下：
+
+```yaml
+
+- name: OpenWrt Packaging
+  uses: ophub/flippy-openwrt-actions@main
+  with:
+    mode: flippy
+  env:
+    OPENWRT_ARMVIRT: openwrt/bin/targets/*/*/*.tar.gz
+    PACKAGE_SOC: s905d_s905x3
+    KERNEL_VERSION_NAME: 5.10.31.TF_5.4.113
+
+```
+
+## 可选参数说明
+
+根据 `Flippy` 最新发布的内核打包脚本，对 `打包文件`、`make.env`、`选择内核版本`、`选择盒子SoC` 等进行了可选参数配置。
+
+| 参数                   | 默认值                                     | 说明                                                    |
+|------------------------|-------------------------------------------|--------------------------------------------------------|
+| OPENWRT_ARMVIRT_PATH   | no                                        | 必选项. 设置 `openwrt-armvirt-64-default-rootfs.tar.gz` 的文件路径，可以使用相对路径如 `openwrt/bin/targets/*/*/*.tar.gz` 或 `http` 开始的公网文件下载地址。 |
+| SCRIPT_REPO_URL        | https://github.com/unifreq/openwrt_packit | 设置打包脚本源码仓库                                     |
+| SCRIPT_REPO_BRANCH     | master                                    | 设置打包脚本源码仓库的分支                                |
+| PACKAGE_SOC            | all                                       | 设置打包盒子的 `SOC` ，默认 `all` 打包全部盒子，可指定单个盒子如 `s905x3` ，可选择多个盒子用_连接如 `s905x3_s905d` |
+| KERNEL_VERSION_NAME    | 5.4.108_5.10.26.TF                        | 设置内核版本，ophub 的 [kernel](https://github.com/ophub/amlogic-s9xxx-openwrt/tree/main/amlogic-s9xxx/amlogic-kernel/kernel) 库里收藏了众多 Flippy 的原版内核，可以查看并选择指定。 |
+| GZIP_IMGS_VALUE        | true                                      | 设置打包完毕是否自动压缩为 .img.gz 文件 (压缩包上传下载更快) |
+| SCRIPT_VPLUS_FILE      | mk_h6_vplus.sh                            | 设置打包 `h6 vplus` 的脚本文件名 |
+| SCRIPT_BEIKEYUN_FILE   | mk_rk3328_beikeyun.sh                     | 设置打包 `rk3328 beikeyun` 的脚本文件名 |
+| SCRIPT_L1PRO_FILE      | mk_rk3328_l1pro.sh                        | 设置打包 `rk3328 l1pro` 的脚本文件名 |
+| SCRIPT_S905_FILE       | mk_s905_mxqpro+.sh                        | 设置打包 `s905 mxqpro+` 的脚本文件名 |
+| SCRIPT_S905D_FILE      | mk_s905d_n1.sh                            | 设置打包 `s905d n1` 的脚本文件名 |
+| SCRIPT_S905X2_FILE     | mk_s905x2_x96max.sh                       | 设置打包 `s905x2 x96max` 的脚本文件名 |
+| SCRIPT_S905X3_FILE     | mk_s905x3_multi.sh                        | 设置打包 `s905x3 multi` 的脚本文件名 |
+| SCRIPT_S912_FILE       | mk_s912_zyxq.sh                           | 设置打包 `s912 zyxq` 的脚本文件名 |
+| SCRIPT_S022X_FILE      | mk_s922x_gtking.sh                        | 设置打包 `s922x gtking` 的脚本文件名 |
+| WHOAMI_VALUE           | flippy                                    | 设置 `make.env` 中 `WHOAMI` 参数的值  |
+| OPENWRT_VER_VALUE      | R21.4.18                                  | 设置 `make.env` 中 `OPENWRT_VER` 参数的值  |
+| SFE_FLAG_VALUE         | 0                                         | 设置 `make.env` 中 `SFE_FLAG` 参数的值  |
+| FLOWOFFLOAD_FLAG_VALUE | 1                                         | 设置 `make.env` 中 `FLOWOFFLOAD_FLAG` 参数的值  |
+
+💡 一般情况下使用默认参数即可，你也可以根据需要进行配置。例如在 Flippy 把打包脚本重命名后导致无法找到原默认脚本文件、make.env 中的固件版本号未更新等情况下，你可以使用可选参数进行实时指定及个性化配置。
+
+## 输出参数说明
+
+根据 github.com 的标准输出了 3 个环境变量，方便编译步骤后续使用。
+
+| 参数                            | 默认值                  | 说明                       |
+|--------------------------------|-------------------------|---------------------------|
+| ${{ env.PACKAGED_OUTPUTPATH }} | /opt/openwrt_packit/tmp | 打包后的固件所在文件夹的路径  |
+| ${{ env.PACKAGED_OUTPUTDATE }} | 2021.04.21.1058         | 打包日期                    |
+| ${{ env.PACKAGED_STATUS }}     | success / failure       | 打包状态。成功 / 失败        |
+      
+## 鸣谢
+
+- [OpenWrt](https://github.com/openwrt/openwrt)
+- [coolsnowwolf/lede](https://github.com/coolsnowwolf/lede)
+- [Lienol/openwrt](https://github.com/Lienol/openwrt)
+- [unifreq/openwrt_packit](https://github.com/unifreq/openwrt_packit)
+- [tuanqing/mknop](https://github.com/tuanqing/mknop)
+- [P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)
+
+## License
+
+[LICENSE](https://github.com/ophub/flippy-openwrt-actions/blob/main/LICENSE) © OPHUB
+
---
+++ openwrt_flippy.sh
@@ -0,0 +1,235 @@
+#!/usr/bin/env bash
+#=====================================================================================
+# Description: Automatically Packaged OpenWrt
+# Function: Use Flippy's kernrl files and script to Packaged openwrt
+# Copyright (C) 2020-2021 Flippy
+# Copyright (C) 2020-2021 https://github.com/ophub/flippy-openwrt-actions
+#=====================================================================================
+
+if [[ -z "${OPENWRT_ARMVIRT}" ]]; then
+   echo "The [ OPENWRT_ARMVIRT ] variable must be specified."
+   echo "You can use ${GITHUB_WORKSPACE} relative path: [ openwrt/bin/targets/*/*/*.tar.gz ]"
+   echo "Absolute path can be used: [ https://github.com/.../releases/download/.../openwrt-armvirt-64-default-rootfs.tar.gz ]"
+   echo "You can run this Actions again after setting."
+   exit 1
+fi
+
+# Set the default value
+PACKAGE_OPENWRT=("vplus" "beikeyun" "l1pro" "s905" "s905d" "s905x2" "s905x3" "s912" "s922x")
+SELECT_ARMBIANKERNEL=("5.10.26.TF" "5.4.108")
+KERNEL_REPO="https://github.com/ophub/amlogic-s9xxx-openwrt/trunk/amlogic-s9xxx/amlogic-kernel/kernel"
+MAKE_PATH=${PWD}
+SCRIPT_REPO_URL_VALUE="https://github.com/unifreq/openwrt_packit"
+SCRIPT_REPO_BRANCH_VALUE="master"
+PACKAGE_SOC_VALUE="all"
+KERNEL_VERSION_NAME_VALUE="5.10.26.TF_5.4.108"
+GZIP_IMGS_VALUE="true"
+SELECT_OUTPUTPATH_VALUE="/opt/openwrt_packit/tmp"
+SAVE_OPENWRT_ARMVIRT_VALUE="true"
+
+# Set the default packaging script
+SCRIPT_VPLUS_FILE="mk_h6_vplus.sh"
+SCRIPT_BEIKEYUN_FILE="mk_rk3328_beikeyun.sh"
+SCRIPT_L1PRO_FILE="mk_rk3328_l1pro.sh"
+SCRIPT_S905_FILE="mk_s905_mxqpro+.sh"
+SCRIPT_S905D_FILE="mk_s905d_n1.sh"
+SCRIPT_S905X2_FILE="mk_s905x2_x96max.sh"
+SCRIPT_S905X3_FILE="mk_s905x3_multi.sh"
+SCRIPT_S912_FILE="mk_s912_zyxq.sh"
+SCRIPT_S022X_FILE="mk_s922x_gtking.sh"
+
+# Set make.env related parameters
+WHOAMI_VALUE="flippy"
+OPENWRT_VER_VALUE="R21.4.18"
+SFE_FLAG_VALUE=0
+FLOWOFFLOAD_FLAG_VALUE=1
+
+# Set font color
+blue_font_prefix="\033[34m"
+purple_font_prefix="\033[35m"
+green_font_prefix="\033[32m"
+yellow_font_prefix="\033[33m"
+red_font_prefix="\033[31m"
+font_color_suffix="\033[0m"
+INFO="[${blue_font_prefix}INFO${font_color_suffix}]"
+STEPS="[${purple_font_prefix}STEPS${font_color_suffix}]"
+SUCCESS="[${green_font_prefix}SUCCESS${font_color_suffix}]"
+WARNING="[${yellow_font_prefix}WARNING${font_color_suffix}]"
+ERROR="[${red_font_prefix}ERROR${font_color_suffix}]"
+
+# Specify the default value
+[[ -n "${SCRIPT_REPO_URL}" ]] || SCRIPT_REPO_URL="${SCRIPT_REPO_URL_VALUE}"
+[[ -n "${SCRIPT_REPO_BRANCH}" ]] || SCRIPT_REPO_BRANCH="${SCRIPT_REPO_BRANCH_VALUE}"
+[[ -n "${PACKAGE_SOC}" ]] || PACKAGE_SOC="${PACKAGE_SOC_VALUE}"
+[[ -n "${KERNEL_VERSION_NAME}" ]] || KERNEL_VERSION_NAME="${KERNEL_VERSION_NAME_VALUE}"
+[[ -n "${GZIP_IMGS}" ]] || GZIP_IMGS=${GZIP_IMGS_VALUE}
+[[ -n "${SELECT_OUTPUTPATH}" ]] || SELECT_OUTPUTPATH="${SELECT_OUTPUTPATH_VALUE}"
+[[ -n "${SAVE_OPENWRT_ARMVIRT}" ]] || SAVE_OPENWRT_ARMVIRT="${SAVE_OPENWRT_ARMVIRT_VALUE}"
+
+# Specify the default packaging script
+[[ -n "${SCRIPT_VPLUS}" ]] || SCRIPT_VPLUS="${SCRIPT_VPLUS_FILE}"
+[[ -n "${SCRIPT_BEIKEYUN}" ]] || SCRIPT_BEIKEYUN="${SCRIPT_BEIKEYUN_FILE}"
+[[ -n "${SCRIPT_L1PRO}" ]] || SCRIPT_L1PRO="${SCRIPT_L1PRO_FILE}"
+[[ -n "${SCRIPT_S905}" ]] || SCRIPT_S905="${SCRIPT_S905_FILE}"
+[[ -n "${SCRIPT_S905D}" ]] || SCRIPT_S905D="${SCRIPT_S905D_FILE}"
+[[ -n "${SCRIPT_S905X2}" ]] || SCRIPT_S905X2="${SCRIPT_S905X2_FILE}"
+[[ -n "${SCRIPT_S905X3}" ]] || SCRIPT_S905X3="${SCRIPT_S905X3_FILE}"
+[[ -n "${SCRIPT_S912}" ]] || SCRIPT_S912="${SCRIPT_S912_FILE}"
+[[ -n "${SCRIPT_S022X}" ]] || SCRIPT_S022X="${SCRIPT_S022X_FILE}"
+
+# Specify make.env variable
+[[ -n "${WHOAMI}" ]] || WHOAMI="${WHOAMI_VALUE}"
+[[ -n "${OPENWRT_VER}" ]] || OPENWRT_VER="${OPENWRT_VER_VALUE}"
+[[ -n "${SFE_FLAG}" ]] || SFE_FLAG="${SFE_FLAG_VALUE}"
+[[ -n "${FLOWOFFLOAD_FLAG}" ]] || FLOWOFFLOAD_FLAG="${FLOWOFFLOAD_FLAG_VALUE}"
+
+echo -e "${INFO} Welcome to use the OpenWrt packaging tool! \n"
+
+cd /opt
+
+# Server space usage
+echo -e "${INFO} Server space usage before starting to compile:\n$(df -hT ${PWD}) \n"
+
+# clone openwrt_packit repo
+echo -e "${STEPS} Cloning repo into openwrt_packit"
+git clone --depth 1 ${SCRIPT_REPO_URL} -b ${SCRIPT_REPO_BRANCH} openwrt_packit
+echo -e "done. \n"
+
+# Load openwrt-armvirt-64-default-rootfs.tar.gz
+if [[ ${OPENWRT_ARMVIRT} == http* ]]; then
+   echo -e "${STEPS} wget openwrt-armvirt-64-default-rootfs.tar.gz file into openwrt_packit"
+   wget ${OPENWRT_ARMVIRT} -q -P openwrt_packit
+else
+   echo -e "${STEPS} copy openwrt-armvirt-64-default-rootfs.tar.gz file into openwrt_packit"
+   cp -f ${GITHUB_WORKSPACE}/${OPENWRT_ARMVIRT} openwrt_packit
+fi
+sync
+echo -e "done. \n"
+
+# Normal openwrt-armvirt-64-default-rootfs.tar.gz file should not be less than 10MB
+armvirt_rootfs_size=$(ls -l openwrt_packit/openwrt-armvirt-64-default-rootfs.tar.gz 2>/dev/null | awk '{print $5}')
+echo -e "${INFO} armvirt_rootfs_size: ${armvirt_rootfs_size}"
+if [[ "${armvirt_rootfs_size}" -ge "10000000" ]]; then
+   echo -e "${INFO} openwrt_packit/openwrt-armvirt-64-default-rootfs.tar.gz loaded successfully"
+else
+   echo -e "${ERROR} openwrt_packit/openwrt-armvirt-64-default-rootfs.tar.gz failed to load."
+fi
+echo -e "done. \n"
+
+# Load all selected kernels
+[ -d kernel ] || sudo mkdir kernel
+if  [[ -n "${KERNEL_VERSION_NAME}" ]]; then
+    unset SELECT_ARMBIANKERNEL
+    oldIFS=$IFS
+    IFS=_
+    SELECT_ARMBIANKERNEL=(${KERNEL_VERSION_NAME})
+    IFS=$oldIFS
+fi
+
+i=1
+for KERNEL_VAR in ${SELECT_ARMBIANKERNEL[*]}; do
+    echo -e "${INFO} (${i}) ${KERNEL_VAR} Kernel loading..."
+    svn checkout ${KERNEL_REPO}/${KERNEL_VAR} kernel
+    pushd kernel && sudo rm -rf .svn && popd >/dev/null
+    let i++
+done
+sync
+echo -e "${INFO} Package OpenWrt Kernel List: ${SELECT_ARMBIANKERNEL[*]} \n"
+
+# Confirm package object
+if  [[ -n "${PACKAGE_SOC}" && "${PACKAGE_SOC}" != "all" ]]; then
+    unset PACKAGE_OPENWRT
+    oldIFS=$IFS
+    IFS=_
+    PACKAGE_OPENWRT=(${PACKAGE_SOC})
+    IFS=$oldIFS
+fi
+echo -e "${INFO} Package OpenWrt SoC List: ${PACKAGE_OPENWRT[*]} \n"
+
+# Packaged OpenWrt
+echo -e "${STEPS} Start packaging openwrt..."
+k=1
+for KERNEL_VAR in ${SELECT_ARMBIANKERNEL[*]}; do
+
+    KERNEL_VAR=${KERNEL_VAR//.TF/}
+    boot_kernel_file=$( ls kernel/boot-${KERNEL_VAR}* 2>/dev/null | head -n 1 )
+    boot_kernel_file=${boot_kernel_file##*/}
+    boot_kernel_file=${boot_kernel_file//boot-/}
+    boot_kernel_file=${boot_kernel_file//.tar.gz/}
+    echo -e "${INFO} (${k}) KERNEL_VERSION: ${boot_kernel_file}"
+    
+    cd openwrt_packit
+    
+    rm -f make.env
+    cat > make.env <<EOF
+WHOAMI=${WHOAMI}
+OPENWRT_VER=${OPENWRT_VER}
+KERNEL_VERSION="${boot_kernel_file}"
+KERNEL_PKG_HOME="/opt/kernel"
+SFE_FLAG=${SFE_FLAG}
+FLOWOFFLOAD_FLAG=${FLOWOFFLOAD_FLAG}
+EOF
+sync
+
+    echo -e "${INFO} make.env info:"
+    cat make.env
+    
+    i=1
+    for PACKAGE_VAR in ${PACKAGE_OPENWRT[*]}; do
+        {
+            echo -e "${STEPS} (${k}.${i}) Start packaging OpenWrt, SoC is [ ${PACKAGE_VAR} ] and Kernel is [ ${KERNEL_VAR} ]"
+            case "${PACKAGE_VAR}" in
+                  vplus | s1) sudo ./${SCRIPT_VPLUS} ;;
+                  beikeyun | s2) sudo ./${SCRIPT_BEIKEYUN} ;;
+                  l1pro | s3) sudo ./${SCRIPT_L1PRO} ;;
+                  s905 | s4) sudo ./${SCRIPT_S905} ;;
+                  s905d | s5) sudo ./${SCRIPT_S905D} ;;
+                  s905x2 | s6) sudo ./${SCRIPT_S905X2} ;;
+                  s905x3 | s7) sudo ./${SCRIPT_S905X3} ;;
+                  s912 | s8) sudo ./${SCRIPT_S912} ;;
+                  s922x | s9) sudo ./${SCRIPT_S022X} ;;
+                  *) ${WARNING} "Have no this SoC. Skipped."
+                     continue ;;
+            esac
+            echo -e "${SUCCESS} (${k}.${i}) Package openwrt completed."
+            sync
+            
+            if  [[ "${GZIP_IMGS_VALUE}" == "true" ]]; then
+                echo -e "${STEPS} gzip the openwrt*.img files in the tmp folder. \n"
+                cd tmp && gzip *.img && sync && cd ../
+            fi
+            
+            let i++
+        }
+    done
+    cd ../
+    
+    let k++
+done
+echo -e "${SUCCESS} All packaged completed. \n"
+
+echo -e "${STEPS} Output environment variables. \n"
+if  [[ -d openwrt_packit/tmp ]]; then
+
+    cd openwrt_packit/tmp
+
+    if  [[ "${SAVE_OPENWRT_ARMVIRT}" == "true" ]]; then
+        echo -e "${STEPS} copy openwrt-armvirt-64-default-rootfs.tar.gz files into tmp folder. \n"
+        cp -f ../openwrt-armvirt-64-default-rootfs.tar.gz . && sync
+    fi
+    
+    echo -e "${STEPS} Output environment variables. \n"
+    echo "PACKAGED_OUTPUTPATH=${PWD}" >> $GITHUB_ENV
+    echo "PACKAGED_OUTPUTDATE=$(date +"%Y.%m.%d.%H%M")" >> $GITHUB_ENV
+    echo "PACKAGED_STATUS=success" >> $GITHUB_ENV
+    echo -e "${INFO} PACKAGED_OUTPUTPATH files list:"
+    echo -e "$(ls /opt/openwrt_packit/tmp 2>/dev/null) \n"   
+else
+    echo -e "${ERROR} Packaging failed. \n"
+    echo "PACKAGED_STATUS=failure" >> $GITHUB_ENV
+fi
+
+# Server space usage and packaged files
+echo -e "${INFO} Server space usage after compilation:\n$(df -hT ${PWD}) \n"
+echo -e "${STEPS} SThe packaging process has been completed. \n"
+
