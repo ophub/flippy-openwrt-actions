fcc22722ecb5a6ebf0a28c1d3f5e302d9e1a0689
ophub <ophub2020@gmail.com>
2022-12-08 12:43:51 +0000

    Modify kernel prompt

--- openwrt_flippy.sh
+++ openwrt_flippy.sh
@@ -92,6 +92,7 @@ DISTRIB_DESCRIPTION_VALUE="OpenWrt"
 STEPS="[\033[95m STEPS \033[0m]"
 INFO="[\033[94m INFO \033[0m]"
 SUCCESS="[\033[92m SUCCESS \033[0m]"
+PROMPT="[\033[93m PROMPT \033[0m]"
 WARNING="[\033[93m WARNING \033[0m]"
 ERROR="[\033[91m ERROR \033[0m]"
 #
@@ -340,7 +341,7 @@ make_openwrt() {
                 {
                    # Rockchip rk3568 series only support 6.0.y and above kernel
                     [[ -n "$(echo "${PACKAGE_OPENWRT_KERNEL6[@]}" | grep -w "${PACKAGE_VAR}")" && "${KERNEL_VAR:0:1}" -ne "6" ]] && {
-                        echo -e "${STEPS} (${i}.${k}) ${WARNING} ${PACKAGE_VAR} cannot use ${KERNEL_VAR} kernel, skip."
+                        echo -e "${STEPS} (${i}.${k}) ${PROMPT} ${PACKAGE_VAR} cannot use ${KERNEL_VAR} kernel, skip."
                         let k++
                         continue
                     }
