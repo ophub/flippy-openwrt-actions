36c6baff6a174e937adfe2ef69422347e1927e76
ophub <68696949+ophub@users.noreply.github.com>
2026-02-07 13:26:13 +0800

    Adjust kernel version query method

--- openwrt_flippy.sh
+++ openwrt_flippy.sh
@@ -399,7 +399,7 @@ query_kernel() {
                 latest_version="$(
                     curl -fsSL \
                         ${kernel_api}/releases/expanded_assets/kernel_${vb} |
-                        grep -oP "${kernel_verpatch}\.[0-9]+.*(?=\.tar\.gz)" |
+                        grep -oP "${kernel_verpatch}\.[0-9]+.*?(?=\.tar\.gz)" |
                         sort -urV | head -n 1
                 )"
 
